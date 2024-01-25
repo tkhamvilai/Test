8dd75e6cb5f27f4f2ccb44c777c959bfe62523d3
Thanakorn Khamvilai <tpk5401@psu.edu>
2024-01-25 11:39:31 -0500

    1/23

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#define ABC 1
+#define DEF
 
-int main()
-{
-	std::cout << "Hello";
-	return 0;
+
+
+int main() {
+  float i = ABC;
+#if WIN32
+  // this part of code will be compiled only if you're using Windows
+  double x = 2;
+#else
+  // this part of code will be compiled only if you're not using Windows
+  double x = 3;
+#endif
+  double a = 1.1;
+
+
+#if ABC
+  double x;
+#endif
+  int y = 4;
+
+
+#if ABC
+  double x;
+#endif
+  return 0;
 }
